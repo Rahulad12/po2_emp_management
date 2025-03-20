66a3e2bc70fd5e9d7de502cdf2b619e95e002f3c
Rahul Adhikari <rahul077@academiacollege.edu.np>
2025-03-20 08:27:55 +0545

    update in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
-
 export default defineConfig({
-  base: "/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        login: "/src/screen/userLogin.html",
+        register: "/src/screen/userRegister.html",
+        dashboard: "/src/screen/employeeDashboard.html",
+        setting: "/src/screen/employeeSettingDashboard.html",
+      },
+    },
+  },
 });
